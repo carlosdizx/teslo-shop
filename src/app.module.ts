0faa5ed5b2ca424ab06b410e6ed05a6ce3e6b397
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-23 16:30:24 -0500

    :sparkles: configure and connect to database and create file .env.template for example of environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
