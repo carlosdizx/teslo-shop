58007569cfd7c5d0c2aaedec5f78e8cf1b981dab
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-07-01 10:00:30 -0500

    :sparkles: create new helper for validate

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
 import { FilesService } from "./files.service";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { fileFilter } from "./helpers/fileFilter.helper";
 
 @Controller("files")
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post("product")
-  @UseInterceptors( FileInterceptor("file"))
+  @UseInterceptors(
+    FileInterceptor("file", {
+      fileFilter: fileFilter,
+    })
+  )
   public uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) throw new BadRequestException("File is empty");
     return file;
   }
 }
