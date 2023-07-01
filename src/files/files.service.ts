a183338136979a89eca0d067d7f520886f67c2b8
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-07-01 10:51:33 -0500

    :sparkles: rename image in s3 and add path

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { S3Service } from "../common/services/s3.service";
+import { v4 as uuid } from "uuid";
 @Injectable()
 export class FilesService {
   constructor(private readonly s3Service: S3Service) {}
 
   public uploadImage = async (file: Express.Multer.File) => {
-    const upload = await this.s3Service.uploadObject(
-      file.originalname,
-      file.buffer
-    );
-    return { message: "Image uploaded successfully", upload: upload };
+    const fileExtension = file.mimetype.split("/")[1];
+    const fileName = `${uuid()}.${fileExtension}`;
+
+    await this.s3Service.uploadObject(`products/${fileName}`, file.buffer);
+
+    return { message: "Image uploaded successfully", file: fileName };
   };
 }
