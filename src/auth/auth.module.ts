133ab24b665f34fe78e57f0a58cb067b33d07e4a
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-07-11 10:05:26 -0500

    :sparkles: implement jwt passport and auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,26 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import User from "./entities/user.entity";
 import ErrorHandler from "../common/utils/error-handler";
 import EncryptUtil from "../common/utils/encrypt-handler";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import * as process from "process";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({
+      defaultStrategy: "jwt",
+    }),
+
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: "1d",
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, ErrorHandler, EncryptUtil],
   exports: [TypeOrmModule],
