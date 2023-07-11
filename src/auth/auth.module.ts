5b03705c561a093275c4ebdd5c01083b4d168f57
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-07-10 23:56:58 -0500

    :sparkles: create new util for encrypt password and usage for password

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,12 @@ import { AuthController } from "./auth.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import User from "./entities/user.entity";
 import ErrorHandler from "../common/utils/error-handler";
+import EncryptUtil from "../common/utils/encrypt-handler";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService, ErrorHandler],
+  providers: [AuthService, ErrorHandler, EncryptUtil],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,21 +4,26 @@ import { Repository } from "typeorm";
 import User from "./entities/user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import ErrorHandler from "../common/utils/error-handler";
+import EncryptUtil from "../common/utils/encrypt-handler";
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private readonly repository: Repository<User>,
-    private readonly errorHandler: ErrorHandler
+    private readonly errorHandler: ErrorHandler,
+    private readonly encryptUtil: EncryptUtil
   ) {}
   public registerUser = async (createUserDto: CreateUserDto) => {
     try {
       const user = this.repository.create({
         ...createUserDto,
+        password: await this.encryptUtil.encryptPassword(
+          createUserDto.password
+        ),
         fullName: createUserDto.fullName.toUpperCase(),
       });
-      const userSaved = await this.repository.save(user);
-      return { ...userSaved, isActive: undefined, roles: undefined };
+      await this.repository.save(user);
+      return { email: createUserDto.email, fullName: createUserDto.fullName };
     } catch (error) {
       this.errorHandler.handleException(error, "AuthService - registerUser");
     }
