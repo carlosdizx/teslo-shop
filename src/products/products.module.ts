b6470734ae694ea7209da0adc661ad81ed7bfad0
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-07-11 17:57:12 -0500

    :sparkles: add authorization in create, update and delete product

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,9 +4,10 @@ import { ProductsService } from "./products.service";
 import { ProductsController } from "./products.controller";
 import { Product, ProductImage } from "./index";
 import ErrorHandler from "../common/utils/error-handler";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), AuthModule],
   controllers: [ProductsController],
   providers: [ProductsService, ErrorHandler],
 })
